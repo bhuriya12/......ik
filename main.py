01a5589f579cdde8f15cb249bb7a1a3bef73d327
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-11-29 17:29:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message
 import pyrogram
 import tgcrypto
 from p_bar import progress_bar
-from config import *
+from config import  api_id, api_hash, bot_token, auth_users
 from subprocess import getstatusoutput
 import helper
 import logging
